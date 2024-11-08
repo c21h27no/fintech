44033a3f381d7a67f994b772d304b06c3bf21bc5
c21h27no <c21h27no.it@gmail.com>
2024-11-08 13:56:32 +0300

    first

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,15 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	build: {
-		assetsInlineLimit: 0
-	},
+	// build: {
+	// 	assetsInlineLimit: 0
+	// },
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
-	server: {
-		fs: {
-			allow: ['..']
-		}
-	}
+	// server: {
+	// 	fs: {
+	// 		allow: ['..']
+	// 	}
+	// }
 });
