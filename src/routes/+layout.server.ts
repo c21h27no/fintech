1f734f3d91fff3e705ad135cf03c492eef241eac
c21h27no <c21h27no.it@gmail.com>
2024-11-08 15:15:06 +0300

    f

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,5 +1,8 @@
 import type { LayoutServerLoad } from './$types';
 
+export const prerender = true;
+export const ssr = true;
+
 export const load: LayoutServerLoad = async ({ cookies }) => {
 	return {
 		authentication: cookies.get('account')
