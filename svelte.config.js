33135278c3d2d66cb5527ac1d285fba844134082
c21h27no <c21h27no.it@gmail.com>
2024-11-08 13:45:58 +0300

    first

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+import { dev } from '$app/environment';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import path from 'path';
@@ -18,6 +19,9 @@ const config = {
 			precompress: false,
 			strict: false
 		}),
+		paths: {
+			base: dev ? '': process.env.BASE_PATH
+		},
 		csrf: {
 			checkOrigin: true
 		},
